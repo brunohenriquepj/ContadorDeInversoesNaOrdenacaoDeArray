74e28b538471cd095e6de6118dbe4db02ebf04e3
Bruno Pereira <bruno.henriquepj@gmail.com>
2016-07-12 17:00:08 -0300

    TDD red - teste nao passa

--- src/com/contadorinversoes/ContadorInversoes.java
+++ src/com/contadorinversoes/ContadorInversoes.java
@@ -1,6 +1,10 @@
 package com.contadorinversoes;
 
 public class ContadorInversoes {
+
+	public long contarInversoes(int[] a) {
+		return 0;
+	}
 	
 	
 
--- test/com/contadorinversoes/ContadorInversoesTest.java
+++ test/com/contadorinversoes/ContadorInversoesTest.java
@@ -1,5 +1,19 @@
 package com.contadorinversoes;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class ContadorInversoesTest {
 
+	@Test
+	public void deveContarInversoesNaOrdenacaoDeUmArray() throws Exception{
+		ContadorInversoes contador = new ContadorInversoes();
+		
+		int[] a = {2,3,8,6,1};
+		long totalInversoes = contador.contarInversoes(a);
+		
+		assertEquals(5, totalInversoes);
+		
+	}
 }
