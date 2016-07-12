3506248892b25e1b321da27aa86fca276b73a267
Bruno Pereira <bruno.henriquepj@gmail.com>
2016-07-12 20:57:11 -0300

    Testes adicionados

--- test/com/contadorinversoes/ContadorInversoesTest.java
+++ test/com/contadorinversoes/ContadorInversoesTest.java
@@ -7,12 +7,32 @@ import org.junit.Test;
 public class ContadorInversoesTest {
 
 	@Test
-	public void deveContarInversoesNaOrdenacaoDeUmArray() throws Exception{
+	public void deveContarDuasInversoesNaOrdenacaoDeUmArray() throws Exception{
 		ContadorInversoes contador = new ContadorInversoes();
 		
-		int[] a = {2,3,8,6,1};
+		int[] a = {3, 1, 2};
+		long totalInversoes = contador.contarInversoes(a);
+		
+		assertEquals(2, totalInversoes);
+	}
+	
+	@Test
+	public void deveContarCincoInversoesNaOrdenacaoDeUmArray() throws Exception{
+		ContadorInversoes contador = new ContadorInversoes();
+		
+		int[] a = {2, 3, 8, 6, 1};
 		long totalInversoes = contador.contarInversoes(a);
 		
 		assertEquals(5, totalInversoes);
+	}
+	
+	@Test
+	public void deveContarOnzeInversoesNaOrdenacaoDeUmArray() throws Exception{
+		ContadorInversoes contador = new ContadorInversoes();
+		
+		int[] a = {38, 27, 43, -5, 9, 82, 10};
+		long totalInversoes = contador.contarInversoes(a);
+		
+		assertEquals(11, totalInversoes);
 	}
 }
